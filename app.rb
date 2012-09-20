61e7557d509e8fde264bf4cd23393be1e490476c
Aric Walker <aric.walker@gmail.com>
2012-09-19 20:36:45 -0600

    added app.rb

---
+++ app.rb
@@ -0,0 +1,38 @@
+require 'net/http'
+require 'uri'
+require 'open-uri'
+require 'json'
+require 'serialport'
+
+
+def write_serial(command, hue, sat, brt)
+  #params for serial port
+  port_str = "/dev/tty.usbmodemfa131"  #may be different for you
+  baud_rate = 9600
+  data_bits = 8
+  stop_bits = 1
+  parity = SerialPort::NONE
+
+  sp = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity)
+
+  #write command and args to serial port
+  sp.putc command
+  sp.putc hue
+  sp.putc sat
+  sp.putc brt
+
+  sp.close
+
+end
+
+def glowValue(x)
+  value = (-240 * Math.abs( Math.sin(x * 0.01))) + 255
+  console.log(value)
+  return value
+end
+
+
+while true
+
+
+end
